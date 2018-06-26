3b8438fa7f415e5b3d4bdf90b2dc570205e5827b
davidokun <davidokun99@gmail.com>
2018-06-26 16:42:23 -0500

    Analysing and documenting first script from movies data

--- 1-basics/ratings-counter.py
+++ 1-basics/ratings-counter.py
@@ -1,13 +1,22 @@
 from pyspark import SparkConf, SparkContext
 import collections
 
+# Create configuration
 conf = SparkConf().setMaster("local").setAppName("RatingsHistogram")
+
+# Get Spark Context from configuration
 sc = SparkContext(conf=conf)
 
+# Creates and RDD object from the data file
 lines = sc.textFile("file:///SparkCourse/ml-100k/u.data")
+
+# Apply a map function and store the result in another RDD object
 ratings = lines.map(lambda x: x.split()[2])
+
+# Then call a terminal operation. In these case result is a tuple
 result = ratings.countByValue()
 
+# Sort values and print result
 sortedResults = collections.OrderedDict(sorted(result.items()))
 for key, value in sortedResults.items():
     print("%s %i" % (key, value))
