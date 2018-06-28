5fc68ff7748d2654906cdc51d180a58196e6b720
davidokun <davidokun99@gmail.com>
2018-06-28 07:46:03 -0500

    Counting words occurrences using flatMap() function

---
+++ 1-basics/word-count.py
@@ -0,0 +1,22 @@
+"""
+Count the times a word appear in a text
+"""
+from pyspark import SparkConf, SparkContext
+
+conf = SparkConf().setMaster("local").setAppName("WordCount")
+sc = SparkContext(conf=conf)
+
+# Read each line in the text
+lines = sc.textFile("file:///sparkcourse/book.txt")
+
+# Split each line by white space and create an unique entry in the RDD
+words = lines.flatMap(lambda x: x.split())
+
+# Count each entry in the RDD
+wordCounts = words.countByValue()
+
+for word, count in wordCounts.items():
+    # Sanitize the encoding to print in console
+    cleanWord = word.encode('ascii', 'ignore')
+    if cleanWord:
+        print(cleanWord.decode() + " " + str(count))
