f0981222442cf6628bd4ac763ac8006698dcb995
davidokun <davidokun99@gmail.com>
2018-07-09 09:06:36 -0500

    Similar Movies - Filter duplicates and compute similarities

--- 2-advance/movie-similarities.py
+++ 2-advance/movie-similarities.py
@@ -26,3 +26,34 @@ def make_pairs(user_ratings):
     (movie1, rating1) = ratings[0]
     (movie2, rating2) = ratings[1]
     return (movie1, movie2), (rating1, rating2)
+
+
+def filter_duplicates(user_ratings):
+    """
+    Remove the duplicates for movies and ratings
+    :param user_ratings:
+    :return:
+    """
+    ratings = user_ratings[1]
+    (movie1, rating1) = ratings[0]
+    (movie2, rating2) = ratings[1]
+    return movie1 < movie2
+
+
+def compute_cosine_similarity(rating_pairs):
+    numPairs = 0
+    sum_xx = sum_yy = sum_xy = 0
+    for ratingX, ratingY in rating_pairs:
+        sum_xx += ratingX * ratingX
+        sum_yy += ratingY * ratingY
+        sum_xy += ratingX * ratingY
+        numPairs += 1
+
+    numerator = sum_xy
+    denominator = sqrt(sum_xx) * sqrt(sum_yy)
+
+    score = 0
+    if (denominator):
+        score = (numerator / (float(denominator)))
+
+    return (score, numPairs)
